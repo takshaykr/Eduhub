ccaf33edbc186dabe59801ce3713e64e1bf88752
takshaykr <168513928+takshaykr@users.noreply.github.com>
2024-05-02 14:34:57 +0530

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoUrl: "mongodb+srv://alexstone4006:RhNEemAr6jXo0z0t@cluster0.bxiu2xf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    Jwt_secret: "belachiobelachio"
+
+};
